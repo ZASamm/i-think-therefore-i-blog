03157c52c2dc7d46ca14b33509e27b05285e3d32
Zara Samm <zarasamm1996@gmail.com>
2024-11-15 14:26:22 +0000

    fix about page indentation

--- about/views.py
+++ about/views.py
@@ -11,10 +11,10 @@ def about_me(request):
     Renders the About page
     """
     if request.method == "POST":
-         collaborate_form = CollaborateForm(data=request.POST)
-    if collaborate_form.is_valid():
-        collaborate_form.save()
-        messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.") 
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.") 
         
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
@@ -22,8 +22,6 @@ def about_me(request):
     return render(
         request,
         "about/about.html",
-        {
-            "about": about,
-            "collaborate_form": collaborate_form,
-          },
+        {"about": about,
+        "collaborate_form": collaborate_form,},
     )
